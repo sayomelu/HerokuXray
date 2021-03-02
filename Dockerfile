07b6ee9289b6e1236d8c98b33e8f1f4e4d7680ea
sayo melu <hi.sayo@outlook.com>
2021-03-02 16:12:43 +0800

    change

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM alpine
 RUN apk add --no-cache ca-certificates caddy &&\
     wget -O - https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip | unzip - &&\
     chmod +x /xray
-ADD etc/start.sh /start.sh
+ADD ./etc/start.sh /start.sh
 RUN chmod +x /start.sh
 
 CMD /start.sh
--- etc/start.sh
+++ etc/start.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$UUID/$UUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $UUID)/g" > /etc/caddy/Caddyfile
-wget -qO- $CONFIGXRAY | sed -e "s/\$UUID/$UUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" > /xray.json
+sed -e "s/\$UUID/$UUID/g" /xray.json
 
 /xray -config /xray.json
-caddy run --config /etc/caddy/caddyfile
